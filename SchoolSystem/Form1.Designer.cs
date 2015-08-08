87a143bbc3d8d3a989b03163aa5b3129aa043244
Junaid Sarfraz <juneed.sarfraz@gmail.com>
2015-08-08 15:18:48 +0500

    This is Initial Commit

---
+++ SchoolSystem/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace SchoolSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("New Admission");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Class Wise Strength");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Left Students Details");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Attandance");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Absent Report");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("School Leaving certificate");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Student Contacts");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Character Certificate");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Student Management", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Class Wise Fee Structure");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Fee Voucher");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Fee Collection Report");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Auto Fee Calculation");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Defaulter List");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Concession Report");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Customizable Fee Structure");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Student Fee History");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Daily / Weekly / Monthly Income Report");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Fine / Annual Charges / Other fee Support");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Fee Change System");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("SMS to Defauulters");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Fee Management", new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16,
+            treeNode17,
+            treeNode18,
+            treeNode19,
+            treeNode20,
+            treeNode21});
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("ClassWise Exam Schedule");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Class Wise Result");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("ClassWise Best Student");
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("TermWise Result Card");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("Result Card Genration");
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Progress Report");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("Overall Result of School");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("DateSheet of Examination");
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Exam Management", new System.Windows.Forms.TreeNode[] {
+            treeNode23,
+            treeNode24,
+            treeNode25,
+            treeNode26,
+            treeNode27,
+            treeNode28,
+            treeNode29,
+            treeNode30});
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Attandance");
+            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Staff Registration");
+            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Monthly Attandance Report");
+            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Employee Salary ");
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Staff Salary Sheet");
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("Experience Certificate");
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("Time Table of Employees");
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("Staff Management", new System.Windows.Forms.TreeNode[] {
+            treeNode32,
+            treeNode33,
+            treeNode34,
+            treeNode35,
+            treeNode36,
+            treeNode37,
+            treeNode38});
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("Expense");
+            System.Windows.Forms.TreeNode treeNode41 = new System.Windows.Forms.TreeNode("Income");
+            System.Windows.Forms.TreeNode treeNode42 = new System.Windows.Forms.TreeNode("Payment Management", new System.Windows.Forms.TreeNode[] {
+            treeNode40,
+            treeNode41});
+            System.Windows.Forms.TreeNode treeNode43 = new System.Windows.Forms.TreeNode("Balance Sheet");
+            System.Windows.Forms.TreeNode treeNode44 = new System.Windows.Forms.TreeNode("Accounts Management", new System.Windows.Forms.TreeNode[] {
+            treeNode42,
+            treeNode43});
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(933, 499);
+            this.splitContainer1.SplitterDistance = 252;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "New Admission";
+            treeNode2.Name = "Node3";
+            treeNode2.Text = "Class Wise Strength";
+            treeNode3.Name = "Node4";
+            treeNode3.Text = "Left Students Details";
+            treeNode4.Name = "Node5";
+            treeNode4.Text = "Attandance";
+            treeNode5.Name = "Node6";
+            treeNode5.Text = "Absent Report";
+            treeNode6.Name = "Node7";
+            treeNode6.Text = "School Leaving certificate";
+            treeNode7.Name = "Node8";
+            treeNode7.Text = "Student Contacts";
+            treeNode8.Name = "Node9";
+            treeNode8.Text = "Character Certificate";
+            treeNode9.BackColor = System.Drawing.Color.Transparent;
+            treeNode9.Checked = true;
+            treeNode9.ForeColor = System.Drawing.Color.Black;
+            treeNode9.Name = "Btn_Student_Management";
+            treeNode9.Text = "Student Management";
+            treeNode10.Name = "Node11";
+            treeNode10.Text = "Class Wise Fee Structure";
+            treeNode11.Name = "Node12";
+            treeNode11.Text = "Fee Voucher";
+            treeNode12.Name = "Node13";
+            treeNode12.Text = "Fee Collection Report";
+            treeNode13.Name = "Node14";
+            treeNode13.Text = "Auto Fee Calculation";
+            treeNode14.Name = "Node15";
+            treeNode14.Text = "Defaulter List";
+            treeNode15.Name = "Node16";
+            treeNode15.Text = "Concession Report";
+            treeNode16.Name = "Node17";
+            treeNode16.Text = "Customizable Fee Structure";
+            treeNode17.Name = "Node18";
+            treeNode17.Text = "Student Fee History";
+            treeNode18.Name = "Node19";
+            treeNode18.Text = "Daily / Weekly / Monthly Income Report";
+            treeNode19.Name = "Node20";
+            treeNode19.Text = "Fine / Annual Charges / Other fee Support";
+            treeNode20.Name = "Node21";
+            treeNode20.Text = "Fee Change System";
+            treeNode21.Name = "Node22";
+            treeNode21.Text = "SMS to Defauulters";
+            treeNode22.Name = "Node10";
+            treeNode22.Text = "Fee Management";
+            treeNode23.Name = "Node35";
+            treeNode23.Text = "ClassWise Exam Schedule";
+            treeNode24.Name = "Node36";
+            treeNode24.Text = "Class Wise Result";
+            treeNode25.Name = "Node37";
+            treeNode25.Text = "ClassWise Best Student";
+            treeNode26.Name = "Node38";
+            treeNode26.Text = "TermWise Result Card";
+            treeNode27.Name = "Node39";
+            treeNode27.Text = "Result Card Genration";
+            treeNode28.Name = "Node40";
+            treeNode28.Text = "Progress Report";
+            treeNode29.Name = "Node41";
+            treeNode29.Text = "Overall Result of School";
+            treeNode30.Name = "Node42";
+            treeNode30.Text = "DateSheet of Examination";
+            treeNode31.BackColor = System.Drawing.Color.Transparent;
+            treeNode31.ForeColor = System.Drawing.Color.Black;
+            treeNode31.Name = "Node33";
+            treeNode31.Text = "Exam Management";
+            treeNode32.Name = "Node44";
+            treeNode32.Text = "Attandance";
+            treeNode33.Name = "Node45";
+            treeNode33.Text = "Staff Registration";
+            treeNode34.Name = "Node46";
+            treeNode34.Text = "Monthly Attandance Report";
+            treeNode35.Name = "Node47";
+            treeNode35.Text = "Employee Salary ";
+            treeNode36.Name = "Node48";
+            treeNode36.Text = "Staff Salary Sheet";
+            treeNode37.Name = "Node49";
+            treeNode37.Text = "Experience Certificate";
+            treeNode38.Name = "Node50";
+            treeNode38.Text = "Time Table of Employees";
+            treeNode39.Name = "Node43";
+            treeNode39.Text = "Staff Management";
+            treeNode40.Name = "Node54";
+            treeNode40.Text = "Expense";
+            treeNode41.Name = "Node55";
+            treeNode41.Text = "Income";
+            treeNode42.Name = "Node53";
+            treeNode42.Text = "Payment Management";
+            treeNode43.Name = "Node56";
+            treeNode43.Text = "Balance Sheet";
+            treeNode44.Name = "Node52";
+            treeNode44.Text = "Accounts Management";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode9,
+            treeNode22,
+            treeNode31,
+            treeNode39,
+            treeNode44});
+            this.treeView1.Size = new System.Drawing.Size(252, 499);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel2_Paint);
+            this.splitContainer2.Size = new System.Drawing.Size(677, 499);
+            this.splitContainer2.SplitterDistance = 29;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(677, 29);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 499);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SchoolSystem/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
