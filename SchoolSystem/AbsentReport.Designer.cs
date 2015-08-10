df031ccba3837ff88b82e1a798f80e01ba9b9e2f
Junaid Sarfraz <juneed.sarfraz@gmail.com>
2015-08-10 15:36:20 +0500

    Absent Report is Implemented in it with initial understanding

--- SchoolSystem/AbsentReport.Designer.cs
+++ SchoolSystem/AbsentReport.Designer.cs
@@ -28,25 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
+            this.BtnGenerateAbsentReport = new System.Windows.Forms.Button();
+            this.AbsentReporTable = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(93, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(191, 49);
+            this.label1.Location = new System.Drawing.Point(14, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "This is Absent Report Screen";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Date";
+            // 
+            // BtnGenerateAbsentReport
+            // 
+            this.BtnGenerateAbsentReport.Location = new System.Drawing.Point(170, 74);
+            this.BtnGenerateAbsentReport.Name = "BtnGenerateAbsentReport";
+            this.BtnGenerateAbsentReport.Size = new System.Drawing.Size(123, 23);
+            this.BtnGenerateAbsentReport.TabIndex = 2;
+            this.BtnGenerateAbsentReport.Text = "Generate Report";
+            this.BtnGenerateAbsentReport.UseVisualStyleBackColor = true;
+            this.BtnGenerateAbsentReport.Click += new System.EventHandler(this.BtnGenerateAbsentReport_Click);
+            // 
+            // AbsentReporTable
+            // 
+            this.AbsentReporTable.AutoScroll = true;
+            this.AbsentReporTable.AutoSize = true;
+            this.AbsentReporTable.ColumnCount = 5;
+            this.AbsentReporTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.AbsentReporTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.AbsentReporTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.AbsentReporTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.AbsentReporTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.AbsentReporTable.Location = new System.Drawing.Point(17, 160);
+            this.AbsentReporTable.Name = "AbsentReporTable";
+            this.AbsentReporTable.RowCount = 2;
+            this.AbsentReporTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.AbsentReporTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.AbsentReporTable.Size = new System.Drawing.Size(73, 10);
+            this.AbsentReporTable.TabIndex = 3;
             // 
             // AbsentReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.Controls.Add(this.AbsentReporTable);
+            this.Controls.Add(this.BtnGenerateAbsentReport);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
             this.Name = "AbsentReport";
-            this.Size = new System.Drawing.Size(672, 150);
+            this.Size = new System.Drawing.Size(672, 271);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +97,10 @@
 
         #endregion
 
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnGenerateAbsentReport;
+        private System.Windows.Forms.TableLayoutPanel AbsentReporTable;
+
     }
 }
--- SchoolSystem/AbsentReport.cs
+++ SchoolSystem/AbsentReport.cs
@@ -11,9 +11,48 @@ namespace SchoolSystem
 {
     public partial class AbsentReport : UserControl
     {
+        static SchoolSystemEntities1 database = new SchoolSystemEntities1();
+        static List<Attandance> getRequiredAttandances(String Date)
+        {
+            List<Attandance> Result = new List<Attandance>();
+            foreach(Attandance A in database.Attandances)
+            {
+                DateTime AttndanceDate = (DateTime)A.AttandanceDate;
+                String AttandaceDateString = AttndanceDate.ToString("dd-MM-yyyy");
+                if(AttandaceDateString.Equals(Date))
+                {
+                    Result.Add(A);
+                }
+            }
+            return Result;
+        }
         public AbsentReport()
         {
             InitializeComponent();
+        }
+
+        private void BtnGenerateAbsentReport_Click(object sender, EventArgs e)
+        {
+            this.AbsentReporTable.Controls.Clear();
+            String SelectedDate = this.dateTimePicker1.Value.ToString("dd-MM-yyyy");
+            List<Attandance> RequiredAttandance = getRequiredAttandances(SelectedDate).Where(x => x.Status == 0).ToList();
+            int RowNnumberTrace = 2;
+            this.AbsentReporTable.Controls.Add(new TextBox() { Text = "Roll Number" }, 0, 0);
+            this.AbsentReporTable.Controls.Add(new TextBox() { Text = "Name", Width = 150}, 1, 0);
+            this.AbsentReporTable.Controls.Add(new TextBox() { Text = "FatherName" , Width = 150 }, 2, 0);
+            this.AbsentReporTable.Controls.Add(new TextBox() { Text = "Contact Number", Width = 150 }, 3, 0);
+            this.AbsentReporTable.Controls.Add(new TextBox() { Text = "Number Of Absents",Width = 180}, 4, 0);
+            this.AbsentReporTable.RowCount++;
+            foreach (Attandance A in RequiredAttandance)
+            {
+                this.AbsentReporTable.Controls.Add(new TextBox() { Text = A.Student.RollNumber }, 0, RowNnumberTrace);
+                this.AbsentReporTable.Controls.Add(new TextBox() { Text = A.Student.Name,Width = 150 }, 1, RowNnumberTrace);
+                this.AbsentReporTable.Controls.Add(new TextBox() { Text = A.Student.FatherName, Width = 150 }, 2, RowNnumberTrace);
+                this.AbsentReporTable.Controls.Add(new TextBox() { Text = A.Student.PhoneNumber, Width = 150 }, 3, RowNnumberTrace);
+                this.AbsentReporTable.Controls.Add(new TextBox() { Text = database.Attandances.Where(x => x.StudentID == A.StudentID).Count().ToString(), Width = 180 }, 4, RowNnumberTrace);
+                this.AbsentReporTable.RowCount++;
+                RowNnumberTrace++;
+            }
         }
     }
 }
