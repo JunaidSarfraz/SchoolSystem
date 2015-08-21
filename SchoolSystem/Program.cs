5f1271de3fb7aadd7e88743dfe171c08336dda96
Junaid Sarfraz <juneed.sarfraz@gmail.com>
2015-08-21 10:07:29 +0500

    Introduce Login Screen. and implement login screen functionality

--- SchoolSystem/Program.cs
+++ SchoolSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolSystem.StartAndLoginScreen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace SchoolSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartScreen());
         }
     }
 }
---
+++ SchoolSystem/StartAndLoginScreen/StartScreen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchoolSystem.StartAndLoginScreen
+{
+    public partial class StartScreen : Form
+    {
+        SchoolSystemEntities1 database = new SchoolSystemEntities1();
+        
+        public StartScreen()
+        {
+            InitializeComponent();
+            this.BtnAccountManagement.Visible = false;
+            this.BtnExamManagement.Visible = false;
+            this.BtnFeeManagement.Visible = false;
+            this.BtnHRManagement.Visible = false;
+            this.BtnStudentManagement.Visible = false;
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            String UserName = this.UserNameTextBox.Text;
+            String Password = this.PasswordTextBox.Text;
+
+            if (database.Users.Where(X => X.UserName.Equals(UserName) && X.Password.Equals(Password)).First() == null)
+            {
+                MessageBox.Show("Username or password is not correct");
+            }
+            else
+            {
+                this.PasswordTextBox.Visible = false;
+                this.UserNameTextBox.Visible = false;
+                this.UserNameLabel.Visible = false;
+                this.PasswordLabel.Visible = false;
+                this.BtnLogin.Visible = false;
+
+                this.BtnAccountManagement.Visible = true;
+                this.BtnExamManagement.Visible = true;
+                this.BtnFeeManagement.Visible = true;
+                this.BtnHRManagement.Visible = true;
+                this.BtnStudentManagement.Visible = true;
+                this.Refresh();
+            }
+        }
+    }
+}
